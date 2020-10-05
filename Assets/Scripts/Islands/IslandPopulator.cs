5b7727baf84baac3639e9675665aa6ba87d64e14
ONCGM <ONCGM@noreply.github.com>
2020-10-05 14:20:12 -0300

    Routine update

---
+++ Assets/Scripts/Islands/IslandPopulator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Islands {
+    /// <summary>
+    /// Populates islands with enemies and loot.
+    /// </summary>
+    public class IslandPopulator : MonoBehaviour {
+        [Header("Enemies settings")]
+        [SerializeField] private bool spawnEnemies;
+        [SerializeField] private List<Transform> enemySpawnPositions = new List<Transform>();
+        [SerializeField] private List<GameObject> enemyPrefabs = new List<GameObject>();
+
+        [Header("Loot Settings")] 
+        [SerializeField] private bool spawnLootBox;
+        [SerializeField] private List<Transform> lootSpawnPositions = new List<Transform>();
+        [SerializeField] private List<GameObject> lootPrefabs = new List<GameObject>();
+
+        private void Awake() {
+            
+        }
+    }
+}
