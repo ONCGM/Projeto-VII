30ed73ba88214ae9c455ae5b65a6649659396986
ONCGM <ONCGM@noreply.github.com>
2020-12-03 22:50:17 -0300

    Store begging

--- Assets/Scripts/Store/StoreController.cs
+++ Assets/Scripts/Store/StoreController.cs
@@ -1,18 +1,58 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Entity.Player;
 using UnityEngine;
 
-public class StoreController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+namespace Store {
+    /// <summary>
+    /// Controls the store behaviour and shit.
+    /// </summary>
+    public class StoreController : MonoBehaviour {
+        #pragma warning disable 0649
+        [Header("References")] 
+        [SerializeField] private PlayerController player;
+        [SerializeField] private CanvasGroup mainCanvasGroup;
+        #pragma warning restore 0649
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Gets references and shit.
+        private void Awake() {
+            player = FindObjectOfType<PlayerController>();
+        }
         
+        /// <summary>
+        /// Starts the store sequence.
+        /// Skips a time period and plays the animations for
+        /// the npcs and their behaviour entering in the store.
+        /// </summary>
+        public void OpenStore() {
+            
+        }
+        
+        /// <summary>
+        /// Ends the store sequence.
+        /// Saves the game and plays the animations for
+        /// the npcs and their behaviour leaving the store.
+        /// </summary>
+        public void CloseStore() {
+            
+        }
+
+        #region UI
+        /// <summary>
+        /// Displays the store UI and stops player from moving.
+        /// </summary>
+        public void DisplayStoreUi() {
+            
+        }
+
+        /// <summary>
+        /// Hides the store UI and allows the player to move again.
+        /// </summary>
+        public void HideStoreUi() {
+            
+        }
+        
+        #endregion
     }
 }
