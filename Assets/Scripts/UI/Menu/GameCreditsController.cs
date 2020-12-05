5a7ccf6c5d20264a8fec0e618ca87a0ab0c26939
ONCGM <ONCGM@noreply.github.com>
2020-12-05 01:52:13 -0300

    Credits

--- Assets/Scripts/UI/Menu/GameCreditsController.cs
+++ Assets/Scripts/UI/Menu/GameCreditsController.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using FMOD.Studio;
+using FMODUnity;
 using Game;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -19,10 +21,19 @@ namespace UI.Menu {
         [SerializeField, Range(0.5f, 10f)] private float fadeAnimationDuration = 3f;
         [SerializeField, Range(30f, 350f)] private float creditsAnimationDuration = 60f;
         [SerializeField] private float creditsAnimationFinalPosition = 2000f;
+        [Header("FMOD Values")]
+        [SerializeField, Range(0f, 3f)] private float startingMusicLevel;
+        [SerializeField, Range(0f, 1f)] private float startingFadeOutLevel;
+        [SerializeField, Range(0.1f, 15f)] private float paramAnimationSpeed = 10f;
+        [SerializeField, Range(20f, 350f)] private float musicFadeOutOffset = 32f;
+        [SerializeField, EventRef] private string musicEventName = "event:/Music/Main_Menu_Music";
+        [SerializeField, ParamRef] private string musicLevelParameter = "Main_Menu_Stage";
+        [SerializeField, ParamRef] private string fadeOutParameter = "Fade_Out";
 
         [Header("References")] 
         [SerializeField] private RectTransform creditsHolderRect;
         private CanvasGroup creditsGroup;
+        private EventInstance instance;
         
         
         #pragma warning restore 0649
@@ -32,7 +43,19 @@ namespace UI.Menu {
             creditsGroup = GetComponent<CanvasGroup>();
             SceneManager.sceneLoaded += OnSceneLoaded;
 
-            creditsHolderRect.DOLocalMoveY(creditsAnimationFinalPosition, creditsAnimationDuration).OnComplete(GoBackToLastScene).id = "credits";
+            creditsHolderRect.DOLocalMoveY(creditsAnimationFinalPosition, creditsAnimationDuration).OnComplete(GoBackToLastScene).SetEase(Ease.Linear).id = "credits";
+            
+            Invoke(nameof(FadeMusic), (creditsAnimationDuration - musicFadeOutOffset));
+            
+            instance = RuntimeManager.CreateInstance(musicEventName);
+            instance.start();
+
+            instance.setParameterByName(musicLevelParameter, startingMusicLevel);
+            instance.setParameterByName(fadeOutParameter, startingFadeOutLevel);
+            SetMusicStage(3f);
+            
+            DOTween.To(() => (float) instance.getParameterByName(fadeOutParameter, out var param),
+                       param => instance.setParameterByName(fadeOutParameter, param), 0f, fadeAnimationDuration);
 
             GameMaster.OnCreditsOpen?.Invoke();
             
@@ -43,6 +66,30 @@ namespace UI.Menu {
                         if(SceneManager.GetSceneByBuildIndex(i).isLoaded) SceneManager.UnloadSceneAsync(i);
                     }
                 };
+        }
+
+        /// <summary>
+        /// Fades the credits music.
+        /// </summary>
+        private void FadeMusic() {
+            instance.setParameterByName(fadeOutParameter, 0.12f);
+        }
+        
+        /// <summary>
+        /// Stops the main menu music.
+        /// Also, destroys this Game Object.
+        /// </summary>
+        public void TriggerMusicStop() {
+            DOTween.To(() => (float) instance.getParameterByName(fadeOutParameter, out var param),
+                       param => instance.setParameterByName(fadeOutParameter, param), 1f, fadeAnimationDuration - 1f);
+        }
+        
+        /// <summary>
+        /// Changes the current level of the main menu music.
+        /// </summary>
+        public void SetMusicStage(float value) {
+            DOTween.To(() => (float) instance.getParameterByName(musicLevelParameter, out var param), 
+                       param => instance.setParameterByName(musicLevelParameter, param), value, paramAnimationSpeed);
         }
 
         /// <summary>
@@ -63,12 +110,19 @@ namespace UI.Menu {
         private void OnSceneLoaded(Scene scene, LoadSceneMode loadMode) {
             if(scene.buildIndex == creditsSceneIndex) return;
             
+            TriggerMusicStop();
             DOTween.To(() => creditsGroup.alpha, x => creditsGroup.alpha = x, 0f, fadeAnimationDuration).onComplete =
                 () => {
                     DOTween.Kill("credits");
                     SceneManager.UnloadSceneAsync(creditsSceneIndex);
                     DOTween.Kill(gameObject);
                 };
+        }
+        
+        // Releases FMOD resources.
+        private void OnDestroy() {
+            instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            instance.release();
         }
     }
 }
